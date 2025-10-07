4e9f31268da0af352abc4482073b9b1fa7b12e62
Eren T. <102971562+99eren99@users.noreply.github.com>
2025-10-07 19:37:54 +0300

    Add files via upload

---
+++ example_tts.py
@@ -0,0 +1,22 @@
+import torchaudio as ta
+import torch
+from src.chatterbox.mtl_tts import ChatterboxMultilingualTTS
+
+# Automatically detect the best available device
+if torch.cuda.is_available():
+    device = "cuda"
+elif torch.backends.mps.is_available():
+    device = "mps"
+else:
+    device = "cpu"
+
+print(f"Using device: {device}")
+
+model = ChatterboxMultilingualTTS.from_checkpoint(
+    "./checkpoints/v1/",
+    device,
+)
+
+text = "Ezreal and Jinx teamed up with Ahri, Yasuo, and Teemo to take down the enemy's Nexus in an epic late-game pentakill."
+wav = model.generate(text)
+ta.save("test-1.wav", wav, model.sr)
