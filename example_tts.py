c175bc2ca0b0f3a6acccc1874c49c8a35fe799e1
Eren T. <102971562+99eren99@users.noreply.github.com>
2025-10-30 13:15:45 +0300

    Implement checkpoint loading for TTS model
    Added functionality to load specific checkpoints for the TTS model.

--- example_tts.py
+++ example_tts.py
@@ -17,6 +17,21 @@ model = ChatterboxMultilingualTTS.from_checkpoint(
     device,
 )
 
+"""
+## or you want to load a specific checkpoint
+model = ChatterboxMultilingualTTS.from_pretrained(device=device)
+checkpoint_path="my_checkpoint"
+
+#for safetensors
+from safetensors.torch import load_file
+t3_state = load_file(checkpoint_path, device="cpu")
+model.t3.load_state_dict(t3_state)
+
+#for torch file
+t3_state  = torch.load(checkpoint_path)
+model.t3.load_state_dict(t3_state)
+"""
+
 text = "Ezreal and Jinx teamed up with Ahri, Yasuo, and Teemo to take down the enemy's Nexus in an epic late-game pentakill."
 wav = model.generate(text, language_id="")
 ta.save("test-1.wav", wav, model.sr)
